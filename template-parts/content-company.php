2c998c8ec1587e5c3b1f84dd7f762f6c5c54d8e8
Tsukasa0105 <tsukasa19950105@gmail.com>
2026-07-17 15:30:55 +0900

    Add phone link to company profile

--- template-parts/content-company.php
+++ template-parts/content-company.php
@@ -14,7 +14,7 @@
     <table class="company-table">
       <tr><th>会社名</th><td>株式会社SOUワークス（SOU Works, Co. Ltd.）</td></tr>
       <tr><th>所在地</th><td>〒750-0065<br>山口県下関市伊崎町二丁目13-34</td></tr>
-      <tr><th>電話／ファックス</th><td>083-223-6472</td></tr>
+      <tr><th>電話／ファックス</th><td><a href="tel:083-223-6472">083-223-6472</a></td></tr>
       <tr><th>メールアドレス</th><td><a href="mailto:k.matsuura@sou-workd.co.jp">k.matsuura@sou-workd.co.jp</a></td></tr>
       <tr><th>事業内容</th><td>・焼酎もろみ、おからなどの未利用資源を原料とした粉末乾燥品の製造<br>・独自の低温乾燥技術（70〜75℃）による機能性成分保持型製品の開発<br>・水産養殖事業者、健康食品メーカー向け原料資材供給</td></tr>
       <tr><th>主要技術</th><td>通気型連続式乾燥機による低温乾燥技術（70〜75℃帯処理）</td></tr>
